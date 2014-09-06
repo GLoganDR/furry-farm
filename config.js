38264060da026a1a1801bbbb3bf6e52db2efa271
David D. Boling <daviddboling@gmail.com>
2014-09-06 17:12:06 -0500

    added changed

--- config.js
+++ config.js
@@ -4,19 +4,19 @@
 
 config.twitter = {
   apiKey      : '2AMFYlJS4C5MXfm2MWRiIjLI4',
-  apiSecret   : 'HPJ0wL9mXtXtOm3Saa3g2NFQMevCdHE0F4UNkw9lB0mkH653jc',
+  apiSecret   : process.env.TWITTER_SECRET,
   callbackUrl : 'http://jessica-vm.com:3334/auth/twitter/callback'
 };
 
 config.google = {
   clientId      : '936106218408-rs6be7fr5ql6hnmj3ktqpigeojfeflmq.apps.googleusercontent.com',
-  clientSecret  : 'Es7ZejJCzpbee7tow8i3kp5i',
+  clientSecret  : process.env.GOOGLE_SECRET,
   callbackUrl   : 'http://jessica-vm.com:3334/auth/google/callback'
 };
 
 config.facebook = {
   clientId      : '792591384125995',
-  clientSecret  : '8fa1b5ceadefeab70a94fb49f0c1cb9f',
+  clientSecret  : process.env.FACEBOOK_SECRET,
   callbackUrl   : 'http://jessica-vm.com:3334/auth/facebook/callback'
 };
 
