d5b51fecae8a34e8df9bcd61e2e3b4c65df10567
Jessica Raines <jessicafraines@gmail.com>
2014-09-05 19:15:16 -0500

    added testing

--- test/unit/message.js
+++ test/unit/message.js
@@ -4,12 +4,13 @@
 'use strict';
 
 var expect    = require('chai').expect,
-    User      = require('../../app/models/user'),
+    Message   = require('../../app/models/message'),
     dbConnect = require('../../app/lib/mongodb'),
     cp        = require('child_process'),
+    Mongo     = require('mongodb'),
     db        = 'furry-farm-test';
 
-describe('User', function(){
+describe('Message', function(){
   before(function(done){
     dbConnect(db, function(){
       done();
@@ -23,21 +24,55 @@ describe('User', function(){
   });
 
   describe('constructor', function(){
-    it('should create a new User object', function(){
-      var u = new User();
-      expect(u).to.be.instanceof(User);
+    it('should create a new Message object', function(){
+      var senderId   = Mongo.ObjectID(),
+          receiverId = Mongo.ObjectID(),
+          body       = 'This is the text of the message',
+          m          = new Message(senderId, receiverId, body);
+      expect(m).to.be.instanceof(Message);
+      expect(m.date).to.be.instanceof(Date);
+      expect(m.senderId).to.be.instanceof(Mongo.ObjectID);
+      expect(m.receiverId).to.be.instanceof(Mongo.ObjectID);
     });
   });
 
-  describe('.displayProfile', function(){
-    it('should display a public-only profile', function(done){
-      var c = '000000000000000000000003';
-      User.displayProfile(c, function(err, user){
-        expect(user.isVisible).to.be.true;
-        expect(user).to.be.ok;
+  describe('.messages', function(){
+    it('should display messages for a given user', function(done){
+      var u = '000000000000000000000002';
+      Message.messages(u, function(err, messages){
+        console.log('######', messages);
+        expect(messages.length).to.equal(1);
         done();
       });
     });
   });
-});
+
+  /*describe('.read', function(){
+    it('should display a single message and mark it as read', function(done){
+      Message.read('a00000000000000000000001', function(message){
+        console.log('&&&&&&&', message);
+        expect(message.isRead).to.be.true;
+        done();
+      });
+    });
+  });*/
+
+  /*describe('.unread', function(){
+    it('should display unread messages', function(done){
+      var message = 'a00000000000000000000002'
+      Messages.unread(u, function(err, message){
+        console.log('^^^^^^^', message);
+      });
+    });
+  });
+
+  describe('iterator', function(){
+    it
+  });*/
+
+
+
+
+
+});//closing brackets
 
